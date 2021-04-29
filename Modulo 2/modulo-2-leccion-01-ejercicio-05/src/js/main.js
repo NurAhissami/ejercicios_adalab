2797e7bd0582c221b05223c8773c2c5b68aa4569
NurAhissami <nurahi10@gmail.com>
2021-04-29 19:19:23 +0200

    Add excercise 04

---
+++ Modulo 2/modulo-2-leccion-01-ejercicio-05/src/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const titleElement = document.querySelector('.title');
+let listElement= document.querySelector ('.name1');
+titleElement.innerHTML = titleElement.innerHTML + listElement.innerHTML;
+let listElement= document.querySelector ('.name2')
