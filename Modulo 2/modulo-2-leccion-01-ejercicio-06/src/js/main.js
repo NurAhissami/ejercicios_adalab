afcaf4cc6aa768267dd190f0ed937393f93d3861
NurAhissami <nurahi10@gmail.com>
2021-04-29 19:58:27 +0200

    Add 06

---
+++ Modulo 2/modulo-2-leccion-01-ejercicio-06/src/js/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const divElement = document.querySelector ('.container');
+const content = '<h1>Lorem ipsum</h1> <img src="http://viaplaceholder.com/350x150"> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+divElement.innerHTML = content;
