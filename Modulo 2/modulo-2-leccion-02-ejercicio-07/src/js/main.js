aa25a23bbe78ee12b5cd95953e78e780e2faaf3a
NurAhissami <nurahi10@gmail.com>
2021-05-03 10:42:29 +0200

    Add excercise 07

---
+++ Modulo 2/modulo-2-leccion-02-ejercicio-07/src/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const title = document.querySelector ('h1');
+const name = 'nur';
+const surname = 'ahissami';
+
+// title.innerHTML = 'hola ' + name + ' bienvenida';
+
+title.innerHTML = `${name} ${surname}`;
+console.log(`hola ${name} ${surname} bienvenida`);
+console.log('nur');
+
+
+
+
+
+
+
