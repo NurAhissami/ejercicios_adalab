13889decf5f363ca7f5f6aac497f0222f2974fe6
NurAhissami <nurahi10@gmail.com>
2021-05-03 09:16:48 +0200

    Add excecise 02

---
+++ Modulo 2/modulo-2-leccion-02-ejercicio-02-1/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const superWave = document.querySelector('.super-wave');
+
+// superWave.classList.add('main__title');
+
+console.log(superWave);
---
+++ Modulo 2/modulo-2-leccion-02-ejercicio-02/src/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const superWave = document.querySelector('.super-wave');
+
+superWave.classList.add('js-superwave');
+// superWave.classList.remove('main');
+
+console.log(superWave);
