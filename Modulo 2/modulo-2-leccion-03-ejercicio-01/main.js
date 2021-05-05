bebc56dd1a9156fd8f855fed83d233d447efecb6
NurAhissami <nurahi10@gmail.com>
2021-05-05 18:16:21 +0200

    Add new excrcise lesson 04

--- Modulo 2/modulo-2-leccion-03-ejercicio-01/main.js
+++ Modulo 2/modulo-2-leccion-03-ejercicio-01/main.js
@@ -3,7 +3,7 @@
 // avatar por defecto
 const DEFAULT_AVATAR = 'http://placehold.it/300x300';
 // avatar que eligió el usuario al registrarse
-let userAvatar = '';
+let userAvatar = 'http://www.fillmurray.com/300/300';
 
 const avatar = document.querySelector('.image');
 const content = `<img src="${DEFAULT_AVATAR}"></img>`;
---
+++ Modulo 2/modulo-2-leccion-04-ejercicio-01/main.js
@@ -0,0 +1,10 @@
+
+const multiplicacion = document.querySelector ('p');
+
+function mult(a,b){
+  return a*b;
+}
+
+multiplicacion.innerHTML= mult(4,3);
+multiplicacion.innerHTML= mult(5,2);
+multiplicacion.innerHTML= mult(2,4);
