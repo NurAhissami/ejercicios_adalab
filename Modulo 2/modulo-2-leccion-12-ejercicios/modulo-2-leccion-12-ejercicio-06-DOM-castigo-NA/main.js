23c12306e75b8fc40c3767b71ef74197a767a7ff
NurAhissami <nurahi10@gmail.com>
2021-05-18 19:45:47 +0200

    Add ex 06

---
+++ Modulo 2/modulo-2-leccion-12-ejercicios/modulo-2-leccion-12-ejercicio-06-DOM-castigo-NA/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/* EJERCICIO 6 */
+const container = document.querySelector(".js-container");
+
+let contentSelect =
+  '<select name="color" id="select-color" class="select-color">';
+contentSelect += '<option value="blanco" class="js-color">Blanco</option>';
+contentSelect += '<option value="azul" class="js-color">Azul</option>';
+contentSelect += '<option value="verde" class="js-color">Verde</option>';
+contentSelect += '<option value="amarillo" class="js-color">Amarillo</option>';
+contentSelect += '<option value="rosa" class="js-color">Rosa</option>';
+contentSelect += "</select>";
+
+container.innerHTML = contentSelect;
+
+function copy100() {
+  let content = `<li>`;
+  for (let i = 0; i < 100; i++) {
+    content += `<p class="text"> He aprendido bien cómo funcionan los bucles ${contentSelect}</p>`;
+  }
+  content += `</li>`;
+  container.innerHTML = content;
+}
+copy100();
+
+const select = document.querySelector(".select-color");
+const options = document.querySelectorAll(".js-color");
+const body = document.querySelector("#body");
+options[0].selected = true;
+
+function changeColor(ev) {
+  if (ev.target.value === "blanco") {
+    body.style.color = "white";
+  } else if (ev.target.value === "azul") {
+    body.style.color = "blue";
+  } else if (ev.target.value === "verde") {
+    body.style.color = "green";
+  } else if (ev.target.value === "amarillo") {
+    body.style.color = "yellow";
+  } else if (ev.target.value === "rosa") {
+    body.style.color = "pink";
+  }
+}
+
+select.addEventListener("change", changeColor);
